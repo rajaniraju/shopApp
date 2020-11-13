058a345e55f8e807e76816d5d79b7cc244191db1
Rajani Raju <rajaniraju2003@gmail.com>
2020-11-13 12:17:02 -0600

    new project

---
+++ ClientApp/src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render () {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <p>Welcome to your new single-page application, built with:</p>
+        <input name="text"></input>
+        <button >Square</button>
+        <button>Weather Forcast</button>
+      </div>
+    );
+  }
+}
