ecf4b1d15a0b70156452772fd6d62f830dbcb956
Rajani Raju <rajaniraju2003@gmail.com>
2020-11-13 12:27:36 -0600

    Value entered in text box can be read

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class Home extends Component {
   static displayName = Home.name;
-
-  render () {
+  state = {
+    value: "",
+  };
+  handleOnChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+  squareOnClick = () => {
+    console.log(this.state.value);
+  };
+  render() {
     return (
       <div>
         <h1>Hello, world!</h1>
         <p>Welcome to your new single-page application, built with:</p>
-        <input name="text"></input>
-        <button >Square</button>
+        <input name="text" onChange={this.handleOnChange}></input>
+        <button onClick={this.squareOnClick}>Square</button>
         <button>Weather Forcast</button>
       </div>
     );
